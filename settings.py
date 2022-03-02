6b634d699596487c6d6f0f3513265892fe16d033
kolllak3 <gurasov12@gmail.com>
2022-03-02 21:29:08 +0800

    add project

---
+++ settings.py
@@ -0,0 +1,49 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion"""
+
+    def __init__(self):
+        """Инициализирует статические настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Настройки корабля
+        self.ship_limit = 3
+
+        # Параметры снаряда
+        self.bullet_width = 3000
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
+
+        # Настройки пришельцев
+        self.fleet_drop_speed = 10
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Инициализирует настройки, изменяющиеся в ходе игры."""
+        self.ship_speed  = 1.5
+        self.bullet_speed = 1.5
+        self.alien_speed = 0.8
+
+        # fleet_direction = 1 обозначает движение вправо;  a -1 - влево
+        self.fleet_direction = 1
+        #self.alien_speed = 0.5
+
+        # Подсчет очков.
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости и стоимость пришельцев."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
